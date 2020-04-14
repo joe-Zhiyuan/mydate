e7cc264027e25e7a28e06a1ec910befcfc5ba69b
joe-Zhiyuan <849975459@qq.com>
2020-04-15 00:21:35 +0800

    webpack多入口配置学习

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -2,16 +2,29 @@
 
 const path = require('path'); // 地址
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // html模板
+// 文件打包会残留上一次打包文件 打包前清空文件夹
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development', // 开发模式
-  entry: path.resolve(__dirname, '../src/main.js'), // 入口文件
+  entry: { // 入口文件 多入口文件
+    main: path.resolve(__dirname, '../src/main.js'),
+    header: path.resolve(__dirname, '../src/header.js')
+  },
   output: {
     filename: '[name].[hash:8].js', // 打包后文件名称 不用每次手动修改文件名
     path: path.resolve(__dirname, '../dist') // 打包后的目录
   },
   plugins:[ // html模板 自动引入js
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../public/index.html')
-    })
+      template: path.resolve(__dirname, '../public/index.html'),
+      filename: 'index.html',
+      chunks: ['main'] // 与入口文件对应的模块名
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/header.html'),
+      filename: 'header.html',
+      chunks: ['header'] //与入口对应的模块名
+    }),
+    new CleanWebpackPlugin() // 打包前清空残留文件
   ]
 }
